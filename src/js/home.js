96fbfd60bca14c560d1ab4bf09cfb9441dc31f33
Terry <hello@terrymun.com>
2016-11-15 18:02:11 +0100

    Using Odometer for displaying statistics

--- src/js/home.js
+++ src/js/home.js
@@ -17,4 +17,15 @@ $(function() {
 
 		$(this).closest('div').siblings(':input[type="search"]').val($(this).data('value'));
 	});
+
+	// Trigger odometer change when scroll position is appropriate
+	var worldMapOdometerUpdated = false;
+	$w.on('scroll resize load', function() {
+		if($w.scrollTop() + 200 > $('#world-map').offset().top + $('#world-map').height() - $w.height()) {
+			$('#world-map').siblings('.col-content').find('.odometer').each(function() {
+				$(this).html($(this).data('target-value'));
+			});
+			worldMapOdometerUpdated = true;
+		}
+	});
 });
