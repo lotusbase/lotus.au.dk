b68279ea3ea38ddf1045b9efa295be73e325c9c3
Terry Mun <hello@terrymun.com>
2016-12-05 02:57:57 +0100

    Finalised rudimentary features for LORE1 View page

---
+++ src/templates/lib/classes/view/source-link.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace LotusBase\View;
+
+/* View\SourceLink */
+class SourceLink {
+
+	private $_vars = array(
+		'external_link' => array(
+			'Gene3D' => 'http://www.cathdb.info/version/latest/superfamily/',
+			'HMMPanther' => 'http://www.pantherdb.org/panther/family.do?clsAccession=',
+			'HMMPfam' => 'http://pfam.xfam.org/family/',
+			'Superfamily' => 'http://supfam.cs.bris.ac.uk/SUPERFAMILY/cgi-bin/scop.cgi?ipid=',
+			'PatternScan' => 'http://prosite.expasy.org/',
+			'ProfileScan' => 'http://prosite.expasy.org/'
+			),
+		'external_id_pattern' => array(
+			'Gene3D' => '/^G3DSA:(.*)$/'
+			),
+		'domain' => array(
+			'HMMPfam' => 'pfam'
+			)
+		);
+
+	// Public function: Set source
+	public function set_source($source) {
+		$this->_vars['Source'] = $source;
+	}
+
+	// Public function: Set ID
+	public function set_id($id) {
+		$this->_vars['SourceID'] = $id;
+	}
+
+	// Private function: Get external link
+	private function get_external_url() {
+		// Variables
+		$source = $this->_vars['Source'];
+		$sourceID = $this->_vars['SourceID'];
+
+		// Set up replacement
+		$pattern = $this->_vars['external_id_pattern'][$source];
+		if(!empty($pattern)) {
+			$url = $this->_vars['external_link'][$source] . preg_replace($pattern, '$1', $sourceID);
+		} else {
+			$url = $this->_vars['external_link'][$source] . $sourceID;
+		}
+
+		return $url;
+	}
+
+	// Public function: Get HTML
+	public function get_html() {
+
+		// Generate output
+		$out = '<div class="dropdown button">';
+		$out .= '<span class="dropdown--title">'.$this->_vars['SourceID'].'</span>';
+		$out .= '<ul class="dropdown--list">';
+		if(array_key_exists($this->_vars['Source'], $this->_vars['domain'])) {
+			$out .= '<li><a href="'.WEB_ROOT.'/api/v1/view/domain/'.$this->_vars['domain'][$this->_vars['Source']].'/'.$this->_vars['SourceID'].'" data-desc-id="'.$this->_vars['SourceID'].'" data-desc-source="'.$this->_vars['domain'][$this->_vars['Source']].'"><span class="icon-eye">Show description</span></a></li>';
+		}
+		if(array_key_exists($this->_vars['Source'], $this->_vars['external_link'])) {
+			$out .= '<li><a href="'.$this->get_external_url().'"><span class="icon-link-ext">View external data</span></a></li>';
+		}
+		$out .= '<li><a href="'.WEB_ROOT.'/tools/trex/?ids='.$this->_vars['SourceID'].'" title="Search for proteins/transripts with this domain"><span class="icon-search">Search for proteins/transripts with this domain</span></a></li>';
+		$out .= '</ul>';
+		$out .= '</div>';
+
+		// Return output
+		return $out;
+	}
+
+}
+
+?>
--- src/templates/view/lore1.php
+++ src/templates/view/lore1.php
@@ -17,7 +17,24 @@
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
+			// Fetch line data
 			$q1 = $db->prepare("SELECT
+				lore.PlantID,
+				lore.Batch,
+				lore.CoordList
+				FROM lore1ins AS lore
+				WHERE lore.PlantID = ? AND lore.Version = '3.0'
+				GROUP BY lore.PlantID
+				LIMIT 1
+				");
+			$q1->execute(array($id));
+			if(!$q1->rowCount()) {
+				throw new Exception('<em>LORE1</em> line does not exist. Please ensure that you have entered a valid line identifier.');
+			}
+			$q1_data = $q1->fetch(PDO::FETCH_ASSOC);
+
+			// Fetch insertion data
+			$q2 = $db->prepare("SELECT
 					lore.PlantID,
 					lore.Batch,
 					lore.ColCoord,
@@ -31,13 +48,53 @@
 					lore.FwPrimer,
 					lore.RevPrimer,
 					lore.PCRInsPos,
-					lore.PCRWT
+					lore.PCRWT,
+					GROUP_CONCAT(DISTINCT gene.Gene ORDER BY gene.Gene) AS Gene,
+					GROUP_CONCAT(DISTINCT exon.Gene ORDER BY exon.Gene) AS Exon,
+					CASE
+						WHEN GROUP_CONCAT(DISTINCT gene.Gene ORDER BY gene.Gene) IS NOT NULL AND GROUP_CONCAT(DISTINCT exon.Gene ORDER BY exon.Gene) IS NULL THEN 'Intronic'
+						WHEN GROUP_CONCAT(DISTINCT exon.Gene ORDER BY exon.Gene) IS NOT NULL THEN 'Exonic'
+						WHEN GROUP_CONCAT(DISTINCT gene.Gene ORDER BY gene.Gene) IS NULL AND GROUP_CONCAT(DISTINCT exon.Gene ORDER BY exon.Gene) IS NULL THEN 'Intergenic'
+						ELSE 'Other'
+					END AS Type
 				FROM lore1ins AS lore
-				WHERE lore.PlantID = ? AND lore.Version = '3.0'");
-			$q1->execute(array($id));
+				LEFT JOIN geneins AS gene ON (
+					lore.Chromosome = gene.Chromosome AND
+					lore.Position = gene.Position AND
+					lore.Orientation = gene.Orientation AND
+					lore.Version = gene.Version
+				)
+				LEFT JOIN exonins AS exon ON (
+					lore.Chromosome = exon.Chromosome AND
+					lore.Position = exon.Position AND
+					lore.Orientation = exon.Orientation AND
+					lore.Version = exon.Version
+				)
+				WHERE lore.PlantID = ? AND lore.Version = '3.0' GROUP BY lore.Salt");
+			$q2->execute(array($id));
 
-			if(!$q1->rowCount()) {
-				throw new Exception('<em>LORE1</em> line does not exist. Please ensure that you have entered a valid line identifier.');
+			// Store data
+			$q2_data = array();
+			$insertion_counts = array(
+				'type' => array(),
+				'chromosome' => array()
+				);
+			while($r = $q2->fetch(PDO::FETCH_ASSOC)) {
+				$q2_data[] = $r;
+
+				// Store insertion types
+				if(!array_key_exists($r['Type'], $insertion_counts['type'])) {
+					$insertion_counts['type'][$r['Type']] = 1;
+				} else {
+					$insertion_counts['type'][$r['Type']] += 1;
+				}
+
+				// Store by chromosome counts
+				if(!array_key_exists($r['Chromosome'], $insertion_counts['chromosome'])) {
+					$insertion_counts['chromosome'][$r['Chromosome']] = 1;
+				} else {
+					$insertion_counts['chromosome'][$r['Chromosome']] += 1;
+				}
 			}
 
 		} else {
@@ -66,7 +123,7 @@
 	?>
 	<link rel="stylesheet" href="<?php echo WEB_ROOT; ?>/dist/css/view.min.css" type="text/css" media="screen" />
 </head>
-<body class="view go init-scroll--disabled">
+<body class="view lore1 init-scroll--disabled">
 
 	<?php
 
@@ -86,55 +143,128 @@
 
 	<section class="wrapper">
 		<h2><?php echo $id; ?></h2>
-		
+		<span class="byline">View in <a href="<?php echo WEB_ROOT.'/lore1/search?pid='.$id.'&v=3.0'; ?>"><em>LORE1</em> search tool</a></span>
+
+		<div id="view__card" class="view__facet">
+			<h3>Overview</h3>
+			<p>The <em>LORE1</em> mutant line <strong><?php echo $id; ?></strong> is from the <?php echo (strpos($q1_data['Batch'], 'DK') > -1 ? 'Danish' : 'Japanese'); ?> batch <?php echo $q1_data['Batch']; ?>, containing <?php echo $q2->rowCount().pl($q2->rowCount(), ' identified insertion'); ?> across the genome. The positions of each insert, <strong>excluding those found in chromosome 0, chloroplast, or mitochondria</strong>, are displayed in the chromosome map below:</p>
+			<div class="d3-chart"><svg id="lore1-by-chromosome" data-lore1="<?php echo $id; ?>"></svg></div>
+		</div>
+
+		<div id="view__insertion-stats" class="view__facet">
+			<h3>Insertion statistics</h3>
+
+			<div id="view__insertion-stats__tabs">
+				<div id="view__insertion-stats__nav" class="cols align-items__flex-end ui-tabs-nav__wrapper">
+					<ul class="tabbed">
+						<li><a href="#view__insertion-stats__by-type" data-custom-smooth-scroll>By type</a></li>
+						<li><a href="#view__insertion-stats__by-chromosome" data-custom-smooth-scroll>By chromosome</a></li>
+					</ul>
+				</div>
+
+				<div class="view__insertion-stats__tab" id="view__insertion-stats__by-type">
+					<p>Insertions can be classified as intergenic or genic, the latter of which can be further subdivided into exonic or intronic in nature.</p>
+					<ul class="cols flex-wrap__nowrap"><?php
+						foreach($insertion_counts['type'] as $type => $count) {
+							echo '<li><span class="count">'.$count.'</span><span class="desc">'.$type.'</span></li>';
+						}
+					?>
+						<li><span class="count"><?php echo $q2->rowCount(); ?></span><span class="desc">Total</span></li>
+					</ul>
+				</div>
+
+				<div class="view__insertion-stats__tab" id="view__insertion-stats__by-chromosome">
+					<p>Distribution of insertions among <em>Lotus</em> v3.0 chromosomes.</p>
+					<ul class="cols flex-wrap__nowrap"><?php
+						foreach($insertion_counts['chromosome'] as $chromosome => $count) {
+							echo '<li><span class="count">'.$count.'</span><span class="desc">'.$chromosome.'</span></li>';
+						}
+					?>
+						<li><span class="count"><?php echo $q2->rowCount(); ?></span><span class="desc">Total</span></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
 		<div id="view__lore1ins" class="view__facet">
 			<h3>Insertion data<?php
-				if($q1->rowCount()) {
-					echo ' <span class="badge">'.$q1->rowCount().'</span>';
+				if($q2->rowCount()) {
+					echo ' <span class="badge">'.$q2->rowCount().'</span>';
 				}
 			?></h3>
-			<p>This table does not show all the data pertaining to each insertion due to space contraints. To view additional columns, please export the dataset.</p>
-			<div class="table-overflow">
-				<table class="table--dense">
-					<thead>
-						<tr>
-							<th scope="col"><abbr title="Chromosome">Chr</abbr></th>
-							<th scope="col"><abbr title="Position">Pos</abbr></th>
-							<th scope="col"><abbr title="Orientation">Orn</abbr></th>
-							<th scope="col">Total coverage</th>
-							<!--<th scope="col">Forward primer</th>
-							<th scope="col">Reverse primer</th>-->
-							<th scope="col">PCR size for WT</th>
-							<th scope="col">PCR size for insert</th>
-							<th scope="col"><abbr title="Coordinates">Coord</abbr></th>
-							<th scope="col">Coordinate list</th>
-							<th scope="col">Coordinate count</th>
-						</tr>
-					</thead>
-					<tbody>
-					<?php while($r = $q1->fetch(PDO::FETCH_ASSOC)) { ?>
-						<tr>
-							<td><?php echo $r['Chromosome']; ?></td>
-							<td><?php echo $r['Position']; ?></td>
-							<td><?php echo $r['Orientation']; ?></td>
-							<td><?php echo $r['TotalCoverage']; ?></td>
-							<!--<td><?php echo $r['FwPrimer']; ?></td>
-							<td><?php echo $r['RevPrimer']; ?></td>-->
-							<td><?php echo $r['PCRWT']; ?></td>
-							<td><?php echo $r['PCRInsPos']; ?></td>
-							<td><?php echo $r['RowCoord'].','.$r['ColCoord']; ?></td>
-							<td><?php echo $r['CoordList']; ?></td>
-							<td><?php echo $r['CoordCount']; ?></td>
-						</tr>
-					<?php } ?>
-					</tbody>
-				</table>
-			</div>
+			<p>The table is wide and may contain columns that are only visible after horizontal scrolling. To view the data in its entirety, we recommend exporting the table as a CSV file.</p>
+			<table class="table--dense">
+				<thead>
+					<tr>
+						<th scope="col"><abbr title="Chromosome">Chr</abbr></th>
+						<th scope="col"><abbr title="Position">Pos</abbr></th>
+						<th scope="col"><abbr title="Orientation">Orn</abbr></th>
+						<th scope="col">Gene</th>
+						<th scope="col">Transcript</th>
+						<th scope="col">Type</th>
+						<th scope="col">Total coverage</th>
+						<th scope="col">Forward primer</th>
+						<th scope="col">Reverse primer</th>
+						<th scope="col">PCR size for WT</th>
+						<th scope="col">PCR size for insert</th>
+						<th scope="col"><abbr title="Coordinates">Coord</abbr></th>
+						<th scope="col">Coordinate list</th>
+						<th scope="col">Coordinate count</th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php foreach ($q2_data as $r) { ?>
+					<tr>
+						<td><?php echo $r['Chromosome']; ?></td>
+						<td><?php echo $r['Position']; ?></td>
+						<td><?php echo $r['Orientation']; ?></td>
+						<td><?php
+							if(!empty($r['Gene'])) {
+								$genes = explode(',', $r['Gene']);
+								echo implode($genes, ' ');
+							} else {
+								echo '&ndash;';
+							}
+						?></td>
+						<td><?php
+							if(!empty($r['Exon'])) {
+								$transcripts = explode(',', $r['Exon']);
+								echo implode($transcripts, ' ');
+							} else {
+								echo '&ndash;';
+							}
+						?></td>
+						<td><?php echo $r['Type']; ?></td>
+						<td><?php echo $r['TotalCoverage']; ?></td>
+						<td><?php echo $r['FwPrimer']; ?></td>
+						<td><?php echo $r['RevPrimer']; ?></td>
+						<td><?php echo $r['PCRWT']; ?></td>
+						<td><?php echo $r['PCRInsPos']; ?></td>
+						<td><?php echo $r['RowCoord'].','.$r['ColCoord']; ?></td>
+						<td><?php echo $r['CoordList']; ?></td>
+						<td><?php echo $r['CoordCount']; ?></td>
+					</tr>
+				<?php } ?>
+				</tbody>
+			</table>
 		</div>
 	</section>
 
 	<?php include(DOC_ROOT.'/footer.php'); ?>
+
+	<!-- Visualisation -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js"></script>
+	<script src="<?php echo WEB_ROOT; ?>/dist/js/plugins/colorbrewer.min.js"></script>
+	<script src="<?php echo WEB_ROOT; ?>/dist/js/plugins/d3-tip.min.js"></script>
+
+	<!-- Tabulation -->
 	<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+	<script src="<?php echo WEB_ROOT; ?>/dist/js/plugins/dataTables/buttons.min.js"></script>
+	<script src="<?php echo WEB_ROOT; ?>/dist/js/plugins/dataTables/buttons-flash.min.js"></script>
+	<script src="<?php echo WEB_ROOT; ?>/dist/js/plugins/dataTables/buttons-html5.min.js"></script>
+	<script src="<?php echo WEB_ROOT; ?>/dist/js/plugins/dataTables/buttons-print.min.js"></script>
+
+	<!-- Functions -->
 	<script src="<?php echo WEB_ROOT; ?>/dist/js/view/lore1.min.js"></script>
 </body>
 </html>
