934045dc140015f94c437ea00ee3e6c18f20ce33
Terry Mun <hello@terrymun.com>
2018-02-14 09:04:40 +0800

    Issue #52: Added SRI checks for externally loaded CDN files

--- src/templates/admin/footer.php
+++ src/templates/admin/footer.php
@@ -8,13 +8,13 @@
 		var access_token = '<?php echo LOTUSBASE_API_KEY; ?>';
 	</script>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-tools/1.2.7/jquery.tools.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.3.2/masonry.pkgd.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/stupidtable/0.0.1/stupidtable.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.3/js.cookie.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-tools/1.2.7/jquery.tools.min.js" integrity="sha384-QhyIsV4cMbD/J98vDzsBwRu1nfO161k77O0WzpGUZRTFwnYL+2lEtFyy3MWDwpbq" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.3.2/masonry.pkgd.min.js" integrity="sha384-uIyWmDKaY12istoLfjqxBAdgS+8VjuAobK7bkGglTK7QOPaeI9iHjvjelPqSmFPM" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js" integrity="sha384-rZfj/ogBloos6wzLGpPkkOr/gpkBNLZ6b6yLy4o+ok+t/SAKlL5mvXLr0OXNi1Hp" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/stupidtable/0.0.1/stupidtable.min.js" integrity="sha384-i1hs1xV885ynDTpLx248kPSNT7iUxg+8qxUH4P5Sm/5G8WDsvIHTh05JlnYudqPl" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js" integrity="sha384-2X//P+hP8e8Kia5lcEJAyW2vaaYxCuSphlrx9A6imNkRUADNZGUMbsf3eQs9E+XJ" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.3/js.cookie.min.js" integrity="sha384-Rf9I5A2cLRyZ6x2mM86zxFJjG9avXrFAYAPa/9iXyNQxCy8CA3lWmA3VOfyCHA9z" crossorigin="anonymous"></script>
 	<script src="./includes/functions.min.js"></script>
 	<!--<script src="/js/ggs.js"></script>-->
 </footer>
